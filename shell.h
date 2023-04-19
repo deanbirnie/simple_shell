563a54c8d7ad090c82e23b4738fb50e8008916a5
Dean Birnie <dean@birnie.co.za>
2023-04-19 05:44:11 -0700

    Added functions to parse input strings for execute function

--- shell.h
+++ shell.h
@@ -11,16 +11,16 @@
 /* Defines the maximum size of an input command */
 #define BUFFER_SIZE 1024
 
-/* Display the prompt for the user to to enter a command */
-void prompt();
-
-/* Entry point of the simple shell program */
-int main(void);
-
-/* Read the user's input command from standard input */
-char *read_input();
+/* do we need this? */
+void prompt(void);
 
 /* Handles the execution of processes and child processes */
 int execute(char **args);
+
+/* used by _strtok to find delimters in a given string */
+char *_strchr(const char *str, int c);
+
+/* used to parse and separate input commands */
+char *_strtok(char *str, const char *delim);
 
 #endif
---
+++ strparse.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * _strchr - searches for the first occurance of a specified character
+ *
+ * @str: the string in which it will search for the character
+ * @c: the character to be searched for
+ *
+ * Return: pointer to the character in the string or NULL if the character
+ * cannot be found
+ */
+char *_strchr(const char *str, int c)
+{
+	while (*str != '\0' && *str != c)
+	{
+		str++;
+	}
+	if (*str == c)
+	{
+		return ((char *)str);
+	}
+	return (NULL);
+}
+
+/**
+ * _strtok - breaks down a string into smaller strings according to
+ * the specified delimiter each time it is called
+ *
+ * @str: the string to be tokenised
+ * @delim: the delimter to use to split up the string
+ *
+ * Description: It is a good idea to create a copy of your string before
+ * calling _strtok as _strtok will modify the string in it's existing memory
+ * location. The first time you call _strtok you must pass both the string and
+ * the delimter (i.e. _strtok(str, delim)). Each time _strtok is called
+ * thereafter, use NULL instead of the string (i.e. _strtok(NULL, delim))
+ * because it stores it's position in the string using a static variable
+ * which persist between function exits.
+ *
+ * Return: a pointer to the token or the start of an array of strings
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *last;
+	char *start;
+
+	if (str != NULL)
+	{
+		last = str;
+	}
+	else
+	{
+		if (last == NULL)
+		{
+			return (NULL);
+		}
+		str = last;
+	}
+	while (*str != '\0' && _strchr(delim, *str) != NULL)
+	{
+		str++;
+	}
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+	start = str;
+	while (*str != '\0' && _strchr(delim, *str) == NULL)
+	{
+		str++;
+	}
+	if (*str != '\0')
+	{
+		*str = '\0';
+		last = str + 1;
+	}
+	else
+	{
+		last = NULL;
+	}
+
+	return (start);
+}
