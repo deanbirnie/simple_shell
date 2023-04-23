cbee0acea13ea9959b429e9c6a3b319ed8910010
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-04-23 06:46:28 -0700

    Added shell.c with better error handling and more functionality

--- shell.c
+++ shell.c
@@ -1,113 +1,112 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#define MAX_INPUT_LENGTH 1024
-#define MAX_TOKENS 100
+#include "shell.h"
 
-void handle_cd(char *path)
+/**
+ * main - Simple Shell
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	if (chdir(path) != 0)
-	{
-		perror("chdir failed");
-	}
+        char *input = NULL;
+        size_t input_len = 0;
+        ssize_t read_len = 0;
+        char *tokens[MAX_TOKENS];
+        int num_tokens = 0, i = 0;
+
+        while (1)
+        {
+                printf("$ ");
+                read_len = getline(&input, &input_len, stdin);
+                if (read_len == -1)
+                {
+                        perror("Error");
+                        break;
+                }
+                if (read_len == 1)
+                {
+                        continue;
+                }
+                input[read_len - 1] = '\0';
+                tokenize_input(input, tokens, &num_tokens);
+                if (strcmp(tokens[0], "exit") == 0)
+                {
+                        handle_exit();
+                }
+                else if (strcmp(tokens[0], "cd") == 0)
+                {
+                        if (num_tokens < 2)
+                        {
+                                fprintf(stderr, "cd: missing argument\n");
+                                continue;
+                        }
+                        handle_cd(tokens[1]);
+                }
+                else
+                {
+                        pid_t pid = fork();
+                        if (pid == -1)
+                        {
+                                perror("Error");
+                        }
+                        else if (pid == 0)
+                        {
+                                execvp(tokens[0], tokens);
+                                perror("Error");
+                                exit(EXIT_FAILURE);
+                        }
+                        else
+                        {
+                                waitpid(pid, &i, 0);
+                        }
+                }
+        }
+        free(input);
+        return (0);
 }
 
-void handle_exit()
-{
-	exit(EXIT_SUCCESS);
-}
-
-int execute_command(char **tokens)
-{
-	pid_t pid = fork();
-
-	if (pid < 0)
-	{
-		perror("fork failed");
-		return -1;
-	}
-
-	if (pid == 0)
-	{
-		execvp(tokens[0], tokens);
-		perror("execvp failed");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		int status;
-		waitpid(pid, &status, 0);
-		return WEXITSTATUS(status);
-	}
-}
-
+/**
+ * tokenize_input - Tokenize the input
+ * @input: Input string
+ * @tokens: Array to store the tokens
+ * @num_tokens: Number of tokens
+ *
+ * Return: void
+ */
 void tokenize_input(char *input, char **tokens, int *num_tokens)
 {
-	char *token = strtok(input, " \t\n");
-	*num_tokens = 0;
+        char *token;
 
-	while (token != NULL && *num_tokens < MAX_TOKENS)
-	{
-		tokens[*num_tokens] = token;
-		*num_tokens += 1;
-		token = strtok(NULL, " \t\n");
-	}
-
-	tokens[*num_tokens] = NULL;
+        *num_tokens = 0;
+        token = strtok(input, " \n\t\r");
+        while (token != NULL && *num_tokens < MAX_TOKENS)
+        {
+                tokens[*num_tokens] = token;
+                *num_tokens += 1;
+                token = strtok(NULL, " \n\t\r");
+        }
+        tokens[*num_tokens] = NULL;
 }
 
-int main()
+/**
+ * handle_cd - Change the current directory
+ * @path: Path of the directory
+ *
+ * Return: void
+ */
+void handle_cd(char *path)
 {
-	char *input = NULL;
-	size_t input_len = 0;
-	ssize_t read_len = 0;
-	char *tokens[MAX_TOKENS];
-	int num_tokens = 0;
+        if (chdir(path) == -1)
+        {
+                perror("Error");
+        }
+}
 
-	while (1)
-	{
-		printf("$ ");
-		read_len = getline(&input, &input_len, stdin);
-
-		if (read_len == -1)
-		{
-			perror("getline failed");
-			continue;
-		}
-
-		if (read_len == 1)
-		{
-			continue;
-		}
-
-		tokenize_input(input, tokens, &num_tokens);
-
-		if (strcmp(tokens[0], "cd") == 0)
-		{
-			if (num_tokens < 2)
-			{
-				fprintf(stderr, "cd: missing argument\n");
-				continue;
-			}
-
-			handle_cd(tokens[1]);
-		}
-		else if (strcmp(tokens[0], "exit") == 0)
-		{
-			handle_exit();
-		}
-		else
-		{
-			execute_command(tokens);
-		}
-	}
-
-	free(input);
-	input = NULL;
-
-	return 0;
+/**
+ * handle_exit - Exit the shell
+ *
+ * Return: void
+ */
+void handle_exit(void)
+{
+        exit(EXIT_SUCCESS);
 }
