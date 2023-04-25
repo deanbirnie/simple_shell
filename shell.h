b5f38ca5f84408e67ccaf6825e11f89a2dd13bec
Dean Birnie <dean@birnie.co.za>
2023-04-25 02:29:23 -0700

    add all function prototypes and definitions

--- shell.h
+++ shell.h
@@ -7,9 +7,14 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* Defines the maximum size of an input command */
 #define BUFFER_SIZE 1024
+#define MAX_INPUT_LENGTH 1024
+#define MAX_TOKENS 100
+
 extern char **environ;
 
 
@@ -17,6 +22,12 @@ void handle_cd(char *path);
 void handle_exit(void);
 int execute_command(char **tokens);
 void tokenize_input(char *input, char **tokens, int *num_tokens);
+
+char *find_command(char *cmd, char *path);
+
+int execute_path(char *cmd, char **args);
+
+int execute_cmd(char *cmd, char **args);
 
 /* do we need this? */
 void prompt(void);
@@ -29,5 +40,10 @@ char *_strchr(const char *str, int c);
 
 /* used to parse and separate input commands */
 char *_strtok(char *str, const char *delim);
+
+/* getline */
+size_t _fread(void *ptr, size_t size, size_t count, FILE *stream);
+int _fgetc(FILE *stream);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif
